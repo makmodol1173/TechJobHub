2611c1777576a7ebc8d5fedf826228d15da942f1
makmodol1173 <makmodol1173@gmail.com>
2024-11-05 22:09:21 +0600

    unused file removed

--- static/JavaScript/F1.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const getStartedBtn = document.querySelector('.get-started-btn');
-    getStartedBtn.addEventListener('click', () => {
-        window.location.href = '../HTML/F2.html';
-    });
-});
