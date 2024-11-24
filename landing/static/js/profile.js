48197a1da9c9f10feb19953544ff47eecd1c2515
makmodol1173 <makmodol1173@gmail.com>
2024-11-24 13:28:47 +0600

    profile and application changes

--- landing/static/js/profile.js
+++ landing/static/js/profile.js
@@ -10,15 +10,31 @@
 
     const profilePictureUpload = document.getElementById('profile-picture-upload');
     const profilePictureContainer = document.querySelector('.profile-picture-container');
+    const profilePicture = profilePictureContainer.querySelector('.profile-picture');
+    const editIcon = profilePictureContainer.querySelector('.edit-icon');
 
     profilePictureUpload.addEventListener('change', function () {
         const file = this.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = function (event) {
-                profilePictureContainer.querySelector('.profile-picture').setAttribute('src', event.target.result);
+                profilePicture.setAttribute('src', event.target.result);
             };
             reader.readAsDataURL(file);
         }
+    });
+
+    editIcon.addEventListener('click', function () {
+        profilePictureUpload.click();
+    });
+
+    const saveButtons = document.querySelectorAll('.save-button');
+    saveButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const parentSection = this.closest('.profile-edit-section');
+            if (parentSection) {
+                parentSection.style.display = 'none';
+            }
+        });
     });
 // });
