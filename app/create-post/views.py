b507a168b816531b9e0dfd809281cb2272a25438
makmodol1173 <makmodol1173@gmail.com>
2024-12-13 02:33:24 +0600

    create post added and show in dashboard

---
+++ app/create-post/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db import connection
+from decouple import config
+
+def create_post(request):
+    auth_token = request.COOKIES.get(config('COOKIE_KEY_1'))
+    role = request.COOKIES.get(config('COOKIE_KEY_2'))
+
+    if not auth_token or not role == 'recruiter':
+      return redirect('/login')
+
+    with connection.cursor() as cursor:
+        query_recruiter = "SELECT recruiter_id FROM recruiter WHERE email = %s"
+        cursor.execute(query_recruiter, (auth_token,))
+        recruiter = cursor.fetchone()
+        
+        if not recruiter:
+          return redirect('/login')
+        recruiter_id = recruiter[0]
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        location = request.POST.get('location')
+        key_responsibilities = request.POST.get('key_responsibilities')
+        educational_requirement = request.POST.get('educational_requirement')
+        year_of_experience = request.POST.get('year_of_experience')
+        deadline = request.POST.get('deadline')
+        job_type = request.POST.get('type')
+
+        with connection.cursor() as cursor:
+            insert_query = """
+                INSERT INTO job_post (
+                    recruiter_id, title, description,
+                    location, key_responsibilities, educational_requirement, year_of_experience, deadline,
+                    type
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(insert_query, (
+                recruiter_id, title, description,
+                location,  key_responsibilities, educational_requirement, year_of_experience, deadline,
+                job_type
+            ))
+            
+        messages.success(request, "Job post created successfully.")
+        return redirect('/dashboard')
+
+    return render(request, 'create-post.html')
