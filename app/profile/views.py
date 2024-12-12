8de91bc25bbed63bc671964970b368c0e8f84f8c
makmodol1173 <makmodol1173@gmail.com>
2024-12-12 20:28:16 +0600

    profile updated

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
+from decouple import config
 
 def profile(request):
-    return render(request, 'profile.html')
+    auth_token = request.COOKIES.get(config('COOKIE_KEY_1'))
+    role = request.COOKIES.get(config('COOKIE_KEY_2'))
+    if not auth_token or not role:
+        return redirect('/login')
+    
+    user = None
+    with connection.cursor() as cursor:
+        query_recruiter = "SELECT * FROM recruiter WHERE email = %s"
+        cursor.execute(query_recruiter, (auth_token,))
+        recruiter_data = cursor.fetchone()
+
+        if recruiter_data:
+            user = {
+                'fname': recruiter_data[1],
+                'lname': recruiter_data[2],
+                'role': role
+            }
+        else:
+            query_job_seeker = "SELECT * FROM job_seeker WHERE email = %s"
+            cursor.execute(query_job_seeker, (auth_token,))
+            job_seeker_data = cursor.fetchone()
+
+            if job_seeker_data:
+                user = {
+                    'fname': job_seeker_data[1],
+                    'lname': job_seeker_data[2],
+                    'role': role 
+                }
+
+    if not user:
+        return redirect('/login')
+
+    return render(request, 'profile.html', user)
