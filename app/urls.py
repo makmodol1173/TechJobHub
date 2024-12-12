9956fde131c715765608c2e37ed34bf1da3ae0fc
makmodol1173 <makmodol1173@gmail.com>
2024-12-12 20:43:20 +0600

    app urls updated

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', include('app.login.urls')),
     path('registration/', include('app.registration.urls')),
     path('profile/', include('app.profile.urls')),
+    path('dashboard/', include('app.dashboard.urls')),
+    path('company-details/', include('app.company-details.urls')),
 ]
