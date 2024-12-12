85c531e4396daf4d0377b84cf15a03f3b55949ec
makmodol1173 <makmodol1173@gmail.com>
2024-12-13 02:33:55 +0600

    dashboard views added

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
+from decouple import config
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    auth_token = request.COOKIES.get(config('COOKIE_KEY_1'))
+    role = request.COOKIES.get(config('COOKIE_KEY_2'))
+
+    if not auth_token or role != 'recruiter':
+        return redirect('/login')
+
+    recruiter_id = None
+    job_posts = []
+
+    with connection.cursor() as cursor:
+        query_recruiter = "SELECT recruiter_id FROM recruiter WHERE email = %s"
+        cursor.execute(query_recruiter, (auth_token,))
+        recruiter = cursor.fetchone()
+
+        if recruiter:
+            recruiter_id = recruiter[0]
+
+    has_posts = False
+    if recruiter_id:
+        with connection.cursor() as cursor:
+            query_jobs = "SELECT title, description, location, key_responsibilities, educational_requirement, year_of_experience, deadline, type FROM job_post WHERE recruiter_id = %s"
+            cursor.execute(query_jobs, (recruiter_id,))
+            job_posts = cursor.fetchall()
+            has_posts = bool(job_posts)
+
+    context = {'job_posts': job_posts, 'has_posts': has_posts}
+    return render(request, 'dashboard.html', context)
