1b7e76230840c8949a1f86ce859c6ce8cfcea70b
makmodol1173 <makmodol1173@gmail.com>
2024-12-13 22:29:38 +0600

    dashboard updated

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -23,10 +23,10 @@ def dashboard(request):
     has_posts = False
     if recruiter_id:
         with connection.cursor() as cursor:
-            query_jobs = "SELECT title, description, location, key_responsibilities, educational_requirement, year_of_experience, deadline, type FROM job_post WHERE recruiter_id = %s"
+            query_jobs = "SELECT title, description, location, key_responsibilities, educational_requirement, year_of_experience, deadline, type, job_post_id FROM job_post WHERE recruiter_id = %s"
             cursor.execute(query_jobs, (recruiter_id,))
             job_posts = cursor.fetchall()
             has_posts = bool(job_posts)
 
-    context = {'job_posts': job_posts, 'has_posts': has_posts}
+    context = {'job_posts': job_posts, 'has_posts': has_posts, 'role':role}
     return render(request, 'dashboard.html', context)
