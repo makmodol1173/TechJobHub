1ccb0c1aa476af4ab7c7566d2bb5b12931f9ad9a
makmodol1173 <makmodol1173@gmail.com>
2024-12-16 23:36:13 +0600

    bookmark views updated

--- app/bookmarks/views.py
+++ app/bookmarks/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseBadRequest
+from decouple import config
 from django.db import connection
 
 def bookmarks(request):
-    job_post_id = request.GET.get('job_post_id')
-    job_seeker_id = request.GET.get('job_seeker_id')
+  auth_token = request.COOKIES.get(config('COOKIE_KEY_1'))
+  role = request.COOKIES.get(config('COOKIE_KEY_2'))
 
-    with connection.cursor() as cursor:
-      query_job_seeker = "SELECT * FROM job_seeker WHERE job_seeker_id = %s"
-      cursor.execute(query_job_seeker, (job_seeker_id,))
-      job_seeker = cursor.fetchone()
-      if job_seeker is None:
-          return redirect(f"/job-posts/{job_post_id}?message='Invalid Request'")
-      
-      query_job_post = "SELECT * FROM job_post WHERE job_post_id = %s"
-      cursor.execute(query_job_post, (job_post_id,))
-      job_post = cursor.fetchone()
-      if job_post is None:
-          return redirect(f"/job-posts/{job_post_id}?message='Invalid Request'")
-      
-      query_application = "SELECT * FROM bookmarks WHERE job_seeker_id = %s & job_post_id = %s"
-      cursor.execute(query_application, (job_seeker_id, job_post_id))
-      application = cursor.fetchone()
-      if application:
-          return redirect(f"/bookmarks/{job_post_id}?message='Already bookmarked'")
-          
-      job_seeker_insert_query = """INSERT INTO bookmarks (job_seeker_id, job_post_id) VALUES (%s, %s)"""
-      cursor.execute(job_seeker_insert_query, (job_seeker_id, job_post_id))
-      connection.commit()
-    return redirect(f"/job-posts/{job_post_id}?message='Applied Successful'")
-      
+  if not auth_token or not role == 'job_seeker':
+    return redirect('/login')
+  
+  job_post_id = request.GET.get('job_post_id')
+  job_seeker_id=None
+  with connection.cursor() as cursor:
+    query_job_seeker = "SELECT job_seeker_id FROM job_seeker WHERE email = %s"
+    cursor.execute(query_job_seeker, (auth_token,))
+    job_seeker = cursor.fetchone()
+    if job_seeker:
+      job_seeker_id = job_seeker[0]
+      print(job_seeker_id)
+            
+    query_job_post = "SELECT job_post_id FROM job_post WHERE job_post_id = %s"
+    cursor.execute(query_job_post, (job_post_id,))
+    job_post = cursor.fetchone()
+    if job_post:
+      job_post_id = job_post[0]
+      print(job_post_id)
+    
+  
+  return render(request, 'bookmarks.html')
