ffacf4ac8f777208ed29873486e0c0235483636c
makmodol1173 <makmodol1173@gmail.com>
2024-12-17 01:25:53 +0600

    bookmark updated

--- app/bookmarks/views.py
+++ app/bookmarks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from decouple import config
 from django.db import connection
 
@@ -17,14 +18,28 @@ def bookmarks(request):
     job_seeker = cursor.fetchone()
     if job_seeker:
       job_seeker_id = job_seeker[0]
-      print(job_seeker_id)
             
     query_job_post = "SELECT job_post_id FROM job_post WHERE job_post_id = %s"
     cursor.execute(query_job_post, (job_post_id,))
     job_post = cursor.fetchone()
     if job_post:
       job_post_id = job_post[0]
-      print(job_post_id)
+   
+    query_bookmark = "SELECT * FROM bookmark WHERE job_seeker_id = %s AND job_post_id = %s"
+    cursor.execute(query_bookmark, (job_seeker_id, job_post_id))
+    bookmark = cursor.fetchone()
+    if bookmark:
+      messages.error(request, "Already Bookmarked")
+      return render(request, 'bookmarks.html')
     
-  
+    print(job_seeker_id)
+    print(job_post_id) 
+    
+    if job_post_id and job_seeker_id:
+      bookmark_insert_query = """INSERT INTO bookmark (job_seeker_id, job_post_id) VALUES (%s, %s)"""
+      cursor.execute(bookmark_insert_query, (job_seeker_id, job_post_id))
+      connection.commit()
+      messages.success(request, "Bookmark added successfully")
+      return render(request, 'bookmarks.html')
+      
   return render(request, 'bookmarks.html')
