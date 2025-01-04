935e3202039b74f32bc0fdb4e03bfbf88e2cdad5
makmodol1173 <makmodol1173@gmail.com>
2025-01-04 13:48:40 +0600

    email setup added

--- techjobhub/settings.py
+++ techjobhub/settings.py
@@ -89,6 +89,12 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'makmodol1173@gmail.com'
+EMAIL_HOST_PASSWORD = 'rbhf dgja sttt aifn'
 
 
 # Password validation
